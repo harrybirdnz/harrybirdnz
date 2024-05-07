89fb689f84d9d68b627d42ca9fd34407432fbd61
harrybirdnz <harrybird.nz@gmail.com>
2024-05-07 17:44:35 +1200

    First commit

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="bg-zinc-100 fixed top-0 w-full z-50 h-12">
+    <ul
+      class="flex flex-row items-center p-2 gap-3 text-sm text-zinc-800 uppercase font-medium tracking-wider whitespace-nowrap mx-auto"
+    >
+      <li class="font-bold">Harry Bird</li>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/education">Education</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-screen bg-[#36454F]">
+    <div class="h-full flex flex-col justify-center items-center">
+      <div class="text-white text-7xl font-bold">Hi, I'm Harry</div>
+      <div class="text-red-500 text-xl font-bold font-serif p-4">
+        A Data Analyst
+      </div>
+    </div>
+  </div>
+  >
+</template>
