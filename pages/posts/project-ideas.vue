475bf4e2c628169f0e063389c3280712446c2025
harrybirdnz <harrybird.nz@gmail.com>
2024-08-06 10:14:31 +1200

    Updated yaml

--- pages/posts/project-ideas.vue
+++ pages/posts/project-ideas.vue
@@ -3,7 +3,7 @@
     <div class="bg-white p-8 shadow-lg rounded w-4/5 mt-20 h-min">
       <h1 class="font-medium text-2xl underline">Ideas</h1>
       <ul class="list-disc pt-5 pl-5">
-        <li>Kinda did them all, woops</li>
+        <li>Self host this website</li>
       </ul>
     </div>
   </div>
