a56bab6acb4d36e21623485eb1c31974152246d9
harrybirdnz <harrybird.nz@gmail.com>
2025-02-24 10:24:21 +1300

    Oops

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
   <div class="min-h-screen bg-[#36454F]">
     <div class="h-full flex flex-col justify-center items-center pt-16">
       <div class="text-white text-7xl font-bold">Hi, I'm Harry</div>
-      <div class="text-red-500 text-xl font-bold font-montserrat p-4">
-        A Data Analyst, Software Developer, and Mechanical Engineer
-      </div>
+      <p class="text-red-500 text-xl font-bold font-montserrat p-4 text-center">
+        A 23 year old Data Analyst, Software Developer, and Mechanical Engineer
+        from NZ
+      </p>
     </div>
   </div>
   >
--- pages/posts/portfolio-website.vue
+++ pages/posts/portfolio-website.vue
@@ -3,11 +3,12 @@
 <template>
   <div class="min-h-screen bg-[#36454F] flex justify-center">
     <div class="bg-white m-10 p-8 shadow-lg rounded w-4/5 mt-20 h-min">
-      <h1 class="font-medium text-2xl underline pb-6">Home Server Project</h1>
+      <h1 class="font-medium text-2xl underline pb-6">Portfolio Website</h1>
       <h2 class="text-xl">Stack</h2>
       <ul class="list-disc list-inside">
         <li>Nuxt</li>
         <li>Vercel</li>
+        <li>LaTex - for CV</li>
       </ul>
     </div>
   </div>
